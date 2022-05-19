e4d213671c4d631302053fc79ef09248313cd182
TJ <tessajleeuwerik@gmail.com>
2022-05-19 12:45:28 +0200

    budget overhaul

--- App.js
+++ App.js
@@ -1,115 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import React from 'react';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
 
 
 function App() {
   return (<Budget/>)
 }
 
-function Expenses() {
-  const [expensesArray, setExpensesArray] = useState([0, 0, 0])
-
-  const updateExpensesArray = (index, newExpense) => {
-    const newExpensesArray = [...expensesArray]
-    newExpensesArray[index] = newExpense
-    setExpensesArray(newExpensesArray)
-    console.log(newExpensesArray.reduce((total, val) => total + val, 0))
-  }
-  
-  const expenseRows = expensesArray.map((expense, i) => <ExpenseRow setExpense={money => updateExpensesArray(i, money)}/>)
-
-  return (
-    <>
-      {expenseRows}
-    </>
-  )
-}
-
-function ExpenseRow(props) {
-  return (
-    <>
-      <Cell/>
-      <MoneyCell setMoney={props.setExpense}/>
-      <Cell/>
-    </>
-  )
-}
+const rows = [
+  1,2
+];
 
 function Cell(props) {
-  
-  return (<input type="text" onChange={event => {
-    const cell = event.target.value
-    props.setCell(cell)
-  }}></input>)
+  return (
+    <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div>
+      <FormControl variant="standard" sx={{ m: 1, mt: 3, width: '25ch' }}>
+          <Input
+            id="standard-adornment-weight"
+            onChange={handleChange('weight')}
+            aria-describedby="standard-weight-helper-text"
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+          />
+        </FormControl>
+      </div>
+    </Box>
+    )
 }
 
 function MoneyCell(props) {
-  
-  return (<input type="text" onChange={event => {
-    const money = event.target.value
-    props.setMoney(money)
-  }}></input>)
+  return (<Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+  <div>
+    <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+      <InputLabel htmlFor="standard-adornment-amount"></InputLabel>
+      <Input
+        id="standard-adornment-amount"
+        onChange={handleChange('amount')}
+        startAdornment={<InputAdornment position="start">€</InputAdornment>}
+      />
+    </FormControl>
+  </div>
+</Box>)
 }
 
 function Total() {
   const [value, setValue] = useState('0')
 
-  const handleClick = (digit) => {
-    if (digit === 'C') {
-      setValue('0')
-    } else if (value === '0') {
-      setValue(digit)
-    } else {
-      setValue(value + digit)
-    }
-  }
-
   return (<input type="text"></input>)
 }
 
-function Budget() {
-  const [value, setValue] = useState('0')
-
-  const handleClick = (digit) => {
-    if (digit === 'C') {
-      setValue('0')
-    } else if (value === '0') {
-      setValue(digit)
-    } else {
-      setValue(value + digit)
-    }
-  }
-
-  return (
-    <div class="mdc-data-table">
-      <table class="mdc-data-table__table" aria-label="Budget">
-        <thead> Expenses </thead>
-        <tr class="mdc-data-table__header-row">
-          <th class="mdc-data-table__header-cell" role="columnheader" scope="col">Date</th>
-          <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Amount</th>
-          <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Type</th>
-        </tr>
-        <tbody class="mdc-data-table__content">
-          <tr class="mdc-data-table__row">
-            <td class="mdc-data-table__cell"><Expenses /></td>
-          </tr>
-          <tr class="mdc-data-table__row">
-            <td class="mdc-data-table__cell">12/05/2022</td>
-            <td class="mdc-data-table__cell">€3</td>
-            <td class="mdc-data-table__cell">coffee</td>
-          </tr>
-          <tr class="mdc-data-table__row">
-            <td class="mdc-data-table__cell">12/05/2022</td>
-            <td class="mdc-data-table__cell">€4.35</td>
-            <td class="mdc-data-table__cell">lunch</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  )
+function InputAdornments() {
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
 }
 
+const handleChange = (prop) => (event) => {
+  const [values, setValues] = useState('0')
+  setValues({ ...values, [prop]: event.target.value });
+};
+
+function Budget() {
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+      <Table sx={{ maxWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Amount</TableCell>
+            <TableCell align="right">Type</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.date}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row"><Cell /></TableCell>
+              <TableCell align="right"><MoneyCell /></TableCell>
+              <TableCell align="right"><Cell /></TableCell>>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
+  )
+
+}
 
 export default App;
