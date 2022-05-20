d75a2f3e5d0db29224a2c218cbd9f94f45029cfb
TJ <tessajleeuwerik@gmail.com>
2022-05-20 14:56:04 +0200

    budget update

--- App.js
+++ App.js
@@ -2,14 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -19,17 +11,15 @@ function App() {
   return (<Budget/>)
 }
 
-const rows = [
-  1,2
-];
-
 function Cell(props) {
+  
+  
   return (
     <div>
       <FormControl variant="standard" sx={{ m: 1, mt: 3, width: '25ch' }}>
           <Input
             id="standard-adornment-weight"
-            onChange={handleChange('weight')}
+            onChange={event => props.onCellChange(event.target.value)}
             aria-describedby="standard-weight-helper-text"
             inputProps={{
               'aria-label': 'weight',
@@ -67,31 +57,45 @@ const handleChange = (prop) => (event) => {
 };
 
 function Budget() {
+  const [budget, setBudget] = useState([{Date: null, Amount: null, Type: null}])
+
+  const setBudgetCell = (index, type, value) => {
+    const newBudget = [...budget]
+    newBudget[index][type] = {...budget[index][type]}
+    newBudget[index][type] = value
+    if (index === budget.length - 1){
+      setBudget([...newBudget, {Date: null, Amount: null, Type: null}])
+    }
+    else {
+      setBudget(newBudget)
+    }
+  }
+
+  const tableRows = budget.map((row, index) => (
+    <tr class="expenses-row">
+        <td class="expenses-cell"><Cell type='Date' onCellChange={(value) => setBudgetCell(index, 'Date', value)}/></td>
+        <td class="expenses-money-cell"><MoneyCell type='Amount' onCellChange={(value) => setBudgetCell(index, 'Amount', value)}/></td>
+        <td class="expenses-cell"><Cell type='Type' onCellChange={(value) => setBudgetCell(index, 'Type', value)}/></td>
+      </tr>
+    ))
 
   return (
     <>
-      <Table sx={{ maxWidth: 400 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="right">Type</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.date}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row"><Cell /></TableCell>
-              <TableCell align="right"><MoneyCell /></TableCell>
-              <TableCell align="right"><Cell /></TableCell>>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
+      <div class="expenses-table">
+      <table class="expenses-table-t" aria-label="Budget">
+        <thead> Expenses </thead>
+        <tr class="expenses-table-headers">
+          <th class="expenses-headers-cell" role="columnheader" scope="col">Date</th>
+          <th class="expenses-headers-cell" role="columnheader" scope="col">Amount</th>
+          <th class="expenses-headers-cell" role="columnheader" scope="col">Type</th>
+        </tr>
+        <tbody class="expenses-content">
+          {tableRows}
+        </tbody>
+      </table>
+      {budget[0].Type}
+    </div>
+  </>
   )
 
 }
